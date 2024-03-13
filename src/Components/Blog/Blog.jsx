92fc131b7222833b3ebc931a1a9d38ad20cd398e
hemelprogramming <himelahmed1442@gmail.com>
2024-03-13 01:31:21 -0600

    my first commit

---
+++ src/Components/Blog/Blog.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+const Blog = () => {
+  const [blog, setBlog] = useState([]);
+  useEffect(() => {
+    fetch('blog.json')
+      .then(res => res.json())
+      .then(data => setBlog(data));
+  }, []);
+
+  return <div></div>;
+};
+
+export default Blog;
